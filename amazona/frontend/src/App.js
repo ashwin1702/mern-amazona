c64e91eba8a4f614675722b1bfe328efa7b61eec
ashwin1702 <joshiashwin9754@gmail.com>
2022-10-13 02:09:17 +0530

    Create-React-App

---
+++ amazona/frontend/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <header >
+        <a href="/">amazona</a>
+        <main>
+          List Product
+        </main>
+      </header>
+    </div>
+  );
+}
+
+export default App;
