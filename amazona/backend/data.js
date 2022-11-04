473a0dc0728102719050a35287f01bd9645096e1
ashwin1702 <joshiashwin9754@gmail.com>
2022-11-04 09:25:03 +0530

    complete Ecommerce

---
+++ amazona/backend/data.js
@@ -0,0 +1,42 @@
+const data={
+    products: [
+        {
+            name:'Nike Slim Shirt',
+            slug:'nike-slim-shirt',
+            image: '/images/p1.jpg',
+            category:'shirts',
+            price:120,
+            countInstock:10,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description:'high quality shirt',
+        },
+        {
+            name:'Nike Slim Shirt',
+            slug:'nike-slpm-shirt',
+            category:'shirts',
+            price:120,
+            image: '/images/p2.jpg',
+            countInstock:10,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description:'high quality shirt',
+        },
+        {
+            name:'Nike Slim Shirt',
+            slug:'nike-slum-shirt',
+            category:'shirts',
+            image: '/images/p3.jpg',
+            price:120,
+            countInstock:10,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description:'high quality shirt',
+        },
+    ]
+}
+
+export default data;
